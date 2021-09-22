c454e94c413b8b8afaa9731279e42f08f23311ac
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-22 21:57:38 +0530

    exercise 2 : alert n prompt

--- code.js
+++ code.js
@@ -1,1 +1,8 @@
-console.log("script two is also working ")
+console.log("script two is also working ");
+//exercise 2
+var name=prompt("give me your username");
+console.log("name is ",name);
+alert("the name of user is "+ name);
+
+// to concatenate string on console use comma(,)
+// to concatenate string on alert use +
