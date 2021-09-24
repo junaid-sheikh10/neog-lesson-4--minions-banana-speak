7750997d76d47c2b5693e541b49ae10e7022826c
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-25 01:14:07 +0530

    output
    input is taken using .value
    output is done using .innerText=""

--- code.js
+++ code.js
@@ -11,9 +11,17 @@ console.log("script two is also working ");
 //exercise 3
 var txtinput=document.querySelector("#text-input");
 var btn=document.querySelector("#junn");
+var div=document.querySelector("#div-out");
+//div.innerText="..loading.."
+
 btn.addEventListener("click",function button(){
-    console.log("clicked")
-    console.log(txtinput.value)
+    
+    console.log("this is in text area"+txtinput.value)
+    
+    var ans=txtinput.value;
+    div.innerText=ans
 })
 
+
+//var t=document.querySelector("#text-area")
 
