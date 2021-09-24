d6614a84618484803ba7edb53d9fbdb6ed253af3
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-24 21:49:49 +0530

    event listener

--- code.js
+++ code.js
@@ -1,8 +1,16 @@
 console.log("script two is also working ");
 //exercise 2
-var name=prompt("give me your username");
-console.log("name is ",name);
-alert("the name of user is "+ name);
+    //var name=prompt("give me your username");
+    //console.log("name is ",name);
+    //alert("the name of user is "+ name);
 
-// to concatenate string on console use comma(,)
-// to concatenate string on alert use +
+    // to concatenate string on console use comma(,)
+    // to concatenate string on alert use +
+
+
+//exercise 3
+var btntranslate = document.querySelector("#btn-translate");
+btntranslate.addEventListener("click",function clickeventhandler()
+{
+    console.log("transalted");
+})
